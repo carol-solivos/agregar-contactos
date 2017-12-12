f1aecb18d575a6b293af7b16e87b49c3a437fe50
carol-solivos <carol.solivos@gmail.com>
2017-12-12 09:42:48 -0400

    modal y traspaso de texto

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
 	$('.modal').modal();
+	$('#save').click(save);
+	function save(){
+	var name = $('#icon_prefix').val();
+	var phone = $('#icon_telephone').val();
+	var mail = $('#icon_mail').val();
+	$('#cards').append('<div class="col s12 m7"><div class="card horizontal"><div class="card-stacked"><div class="card-content"><h4>' + name + '</h4><p>' + phone + '</p><p>' + mail + '</p></div><div class="card-action"><a href="#"><i class="fa fa-trash"></i></a></div></div></div></div>');
+	};
 });
-
